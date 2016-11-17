f0fe7b522f95b6ade12c59c6924e6c273ba17747
gabrielnespoli <gabmesnespoli@gmail.com>
2016-11-17 15:52:22 +0100

    add files of the Gradient Descent into the repository

---
+++ gradientdescent-lib.py
@@ -0,0 +1,53 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import tkinter
+
+def plotGradientDescent(x,y,theta):
+	plt.figure()  
+    #line = [b a]; y = b + ax
+	plt.plot(x[:,1], theta[1]*x[:,1] + theta[0], '-')
+	plt.plot(x[:,1], y, 'o')
+	
+	plt.ylabel('log(price)')
+	plt.xlabel('Average Growing Season Temperature')
+	plt.title("The Wine Equation")	
+	
+	plt.savefig('1743585.png')
+
+def normVector(v):
+	sum = 0.0
+	for i in range(0,len(v)):
+		sum += v[i]**2
+	return math.sqrt(sum)
+
+def stopEvaluation(theta,thetaOld,eps):
+    return not normVector(theta-thetaOld)/normVector(theta) > eps
+
+def grad(y, x, theta):
+    return 2.2026486*np.dot(x.T,(np.dot(x, theta) - y))
+
+def descent(y, x, alpha = 0.02235, itr = 1e2, eps = 0.02):
+    p = x.shape[1]
+    theta = np.zeros(p)
+    thetaOld = np.ones(p)
+    count = 0
+    start = True
+    while start == True or (not stopEvaluation(theta,thetaOld,eps) and count < itr):
+        start = False
+        thetaOld = theta
+        theta = thetaOld - alpha*grad(y,x,thetaOld)
+        count += 1
+    return theta
+
+def r2(y, c, x):
+    N = len(y)
+    yPred = np.dot(x, c)
+    yMean = y.sum()/N
+    SSR = 0
+    SSTO = 0
+    for i in range(0,N):
+        SSR = SSR + (yPred[i] - yMean)**2
+        SSTO = SSTO + (y[i] - yMean)**2
+    r2 = SSR/SSTO
+    return r2
---
+++ test-run.py
@@ -0,0 +1,60 @@
+import csv
+import numpy as np
+import math
+mylib = __import__('1743585-lib')
+import tkinter
+
+def normalize(wines):
+	print(wines)
+	meanWinterRain = np.average(wines[:,2])
+	meanAGST = np.average(wines[:,3])
+	meanHarvestRain = np.average(wines[:,4])
+	meanAge = np.average(wines[:,5])
+	meanFrancePop = np.average(wines[:,6])
+
+	varWinterRain = math.sqrt(sum([(w - meanWinterRain)**2 for w in wines[:,2]])/nLines)
+	varAGST = math.sqrt(sum([(a - meanAGST)**2 for a in wines[:,3]])/nLines)
+	varHarvestRain = math.sqrt(sum([(h - meanHarvestRain)**2 for h in wines[:,4]])/nLines)
+	varAge = math.sqrt(sum([(age - meanAge)**2 for age in wines[:,5]])/nLines)
+	varFrancePop = math.sqrt(sum([(f - meanFrancePop)**2 for f in wines[:,6]])/nLines)
+
+	for l in range(0,nLines):
+		for c in range(0,nColumns):
+			if c == 2:
+				wines[l][c] = (wines[l][c] - meanWinterRain)/varWinterRain
+			if c == 3:
+				wines[l][c] = (wines[l][c] - meanAGST)/varAGST
+			if c == 4:
+				wines[l][c] = (wines[l][c] - meanHarvestRain)/varHarvestRain
+			if c == 5:
+				wines[l][c] = (wines[l][c] - meanAge)/varAge
+			if c == 6:
+				wines[l][c] = (wines[l][c] - meanFrancePop)/varFrancePop
+	
+f = open('./wine.csv')
+winesList = []
+nLines = 0
+nColumns = 0
+next(f)
+for line in csv.reader(f):
+	winesList = winesList + line
+	nLines += 1
+	nColumns = len(line)
+wines = np.array(winesList, dtype='f')
+wines = wines.reshape((nLines,nColumns))
+normalize(wines)
+
+#AGPS
+x = np.c_[ np.ones(nLines), wines[:,3]]
+y = wines[:,1]
+theta = mylib.descent(y, x)
+print(theta)
+print("R2 = " + str(mylib.r2(y,theta,x)))
+mylib.plotGradientDescent(x,y,theta)
+
+#4 VARIABLES
+x = np.c_[ np.ones(nLines), wines[:,2:6]]
+y = wines[:,1]
+theta = mylib.descent(y, x)
+print(theta)
+print("R2 = " + str(mylib.r2(y,theta,x)))
